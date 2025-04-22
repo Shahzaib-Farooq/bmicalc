e3677ddc8b506f706fdac62c5d8f7f65d3d371d1
Shahzaib <ss1635200@gmail.com>
2025-04-22 21:04:24 +0500

    all changes

--- app.py
+++ app.py
@@ -22,15 +22,40 @@ st.title("Data Visualization / Chart")
 
 data = {
     "Product": ["rafay","Umer","adil","huzaifa"],
-    "Sales": [120,90,80,70]
+    "Sales": [120,90,80,70],
+    "Players":["Babar","Rizwan","F.Zaman","Ahmed"],
+    "Scores":[3000,8000,6000,5000]
 }
 df = pd.DataFrame(data)
 st.subheader("Data")
 st.dataframe(df)
 st.subheader("Datax")
 
+# fig, ax = plt.subplots() #figure ax variables
+# ax.bar(df["Product"],df["Sales"])
+# ax.set_ylabel("Sales")
+# ax.set_title("Sales of Fruits")
+# st.pyplot(fig) 
 fig, ax = plt.subplots()
-ax.bar(df["Product"],df["Sales"])
-ax.set_ylabel("Sales")
-ax.set_title("Sales of Fruits")
+ax.bar(df["Players"],df["Scores"])
+ax.set_ylabel("Runs")
+ax.set_title("Players Performance")
+st.pyplot(fig)
+
+st.title("Excel file uploader")
+
+uf = st.file_uploader("Upload File", type=["xlsx"])
+if uf is not None:
+    df = pd.read_excel(uf)
+
+    st.subheader("Excel Data")
+    st.dataframe(df.head(5))
+else:
+    st.info("please upload excel file .xlsx")
+
+fig, ax = plt.subplots()
+ax.bar(df["Products"],df["UnitPrice"])
+ax.set_ylabel("Devices")
+ax.set_xlabel("Product Prices")
+ax.set_title("Devices")
 st.pyplot(fig)
